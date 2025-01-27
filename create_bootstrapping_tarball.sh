7b55b38aed0a26df71ada9b83dcb973d6f678e17
Johannes Maier <johannes.maier@active-group.de>
2025-01-27 12:36:14 +0100

    Create bootstrapping tarball in specified target location

--- create_bootstrapping_tarball.sh
+++ create_bootstrapping_tarball.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-target=$(mktemp)
-tar cJf "$target" .
-cp "$target" nix-starter-kit.tar.xz
+set -e
+
+if [[ $# != 1 ]]; then
+    echo "Usage:  ./$(basename "$0") TARGET_DIR"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Target directory '$1' does not exist"
+    exit 2
+fi
+
+tmp=$(mktemp)
+tar cJf "$tmp" .
+cp "$tmp" "$1"/nix-starter-kit.tar.xz
