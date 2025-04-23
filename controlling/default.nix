3665bb4a0e77df12daabd4bc045ff351ceed8bea
Mike Sperber <sperber@deinprogramm.de>
2025-04-23 11:36:38 +0200

    Update Buchhaltung

--- controlling/default.nix
+++ controlling/default.nix
@@ -2,7 +2,7 @@
 { home.packages =
     let activeControlling = builtins.fetchGit {
           url = "ssh://git@gitlab.active-group.de:1022/ag/buchhaltung-controlling.git";
-          rev = "80fef43d0af028bbaf35ad737e96b09d308246a9";
+          rev = "401c147d635b3db480968562ac674408ba3ed20d";
         };
     in [ (import "${activeControlling}/default.nix").outputs.packages.${pkgs.stdenv.system}.default ];
 }
