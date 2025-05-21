d68f1f82f88a4669b6f9c1b0170d681b531b60c0
Johannes Maier <johannes.maier@active-group.de>
2025-05-21 10:04:40 +0200

    controlling: update

--- controlling/default.nix
+++ controlling/default.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }:
-{ home.packages =
-    let activeControlling = builtins.fetchGit {
-          url = "ssh://git@gitlab.active-group.de:1022/ag/buchhaltung-controlling.git";
-          rev = "401c147d635b3db480968562ac674408ba3ed20d";
-        };
-    in [ (import "${activeControlling}/default.nix").outputs.packages.${pkgs.stdenv.system}.default ];
+
+{
+  home.packages =
+    let
+      activeControlling = builtins.fetchGit {
+        url = "ssh://git@gitlab.active-group.de:1022/ag/buchhaltung-controlling.git";
+        rev = "00e4c8cd5465d1e19be30dfcf8a56801c3e3ab9f";
+      };
+    in
+    [ (import "${activeControlling}/default.nix").outputs.packages.${pkgs.stdenv.system}.default ];
 }
