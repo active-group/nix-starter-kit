953bf8ee6c59dd961caa3ace358de2773d22d1ab
Mike Sperber <sperber@deinprogramm.de>
2025-05-27 11:18:52 +0200

    Bump Nix to 25.05.

--- home.nix
+++ home.nix
@@ -8,11 +8,11 @@ let
   stateVersion = settings.stateVersion or "24.11";
   # NOTE: When updating the setup within the same release channel, put its
   # latest commit here.
-  nixpkgsRev = "fecfeb86328381268e29e998ddd3ebc70bbd7f7c";
+  nixpkgsRev = "101f54e74e5b9969bba239480e2e77c4aac0c3e0";
   nixpkgs =
     settings.nixpkgs or (fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-      sha256 = "0m52nb9p4q468pgi1657dzcpsrxd1f15flxljaplxzjyiwbrzz5f";
+      sha256 = "10y128y7y93p88w7s2d65333yk551jxdg8qdv757yby223jmq20p";
     });
   pkgs = import nixpkgs { };
 in
