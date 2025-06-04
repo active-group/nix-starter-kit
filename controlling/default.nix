568ad6945ab06693dc7b4f66f19bf5b6bc6c50fd
Micha <42565366+micharied@users.noreply.github.com>
2025-06-04 09:17:41 +0200

    Update controlling

--- controlling/default.nix
+++ controlling/default.nix
@@ -5,7 +5,7 @@
     let
       activeControlling = builtins.fetchGit {
         url = "ssh://git@gitlab.active-group.de:1022/ag/active-accounting.git";
-        rev = "7b52eb44115728075dffde2328639fda1546e421";
+        rev = "15db12f74aec7fe4f9ad2841755126da7c51a2cb";
       };
     in
     [ (import "${activeControlling}/default.nix").outputs.packages.${pkgs.stdenv.system}.default ];
