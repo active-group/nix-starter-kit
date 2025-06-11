5a5b9abe7bd0d39f7117744ef7a2b1e15182b903
Mike Sperber <sperber@deinprogramm.de>
2025-06-11 16:15:03 +0200

    Move nixpkgs version to pkgs.nix.
    This is to then feed those same packages into home-manager.
    
    Documentation forthcoming.

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 let
   settings = import ./user-settings.nix;
@@ -9,18 +9,15 @@ let
   # NOTE: When updating the setup within the same release channel, put its
   # latest commit here.
   # NixOS 25.05 as of 2025-05-27
-  nixpkgsRev = "101f54e74e5b9969bba239480e2e77c4aac0c3e0";
-  nixpkgs =
-    settings.nixpkgs or (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-      sha256 = "10y128y7y93p88w7s2d65333yk551jxdg8qdv757yby223jmq20p";
-    });
-  pkgs = import nixpkgs { };
 in
 {
   imports =
     settings.additionalModules settings
-    ++ pkgs.lib.optional pkgs.stdenv.hostPlatform.isDarwin (import ./mac-app-util);
+    # pkgs.stdenv.hostPlatform.isDarwin accesses pkgs too early
+    # and causes infinite recursion
+    ++ lib.optional
+      (builtins.currentSystem == "aarch64-darwin" || builtins.currentSystem == "x86_64-darwin") 
+      (import ./mac-app-util);
 
   home = rec {
     inherit (settings) username;
