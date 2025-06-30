495d80066f059fc479c530d9017a4e0f364dd0a9
Mike Sperber <sperber@deinprogramm.de>
2025-06-30 15:26:14 +0200

    Git as a module.

--- flake.nix
+++ flake.nix
@@ -56,6 +56,46 @@
           make-git = import ./git;
           controlling = import ./controlling;
         };
+        nixosModules.git =
+          {
+            config,
+            pkgs,
+            lib,
+            ...
+          }:
+          let cfg = config.active-group.git;
+          in
+          {
+            options.active-group.git = {
+              enable = lib.mkEnableOption "git";
+              userName = lib.mkOption {
+                type = lib.types.nonEmptyStr;
+              };
+              userEmail = lib.mkOption {
+                type = lib.types.nonEmptyStr;
+              };
+            };
+            config = lib.mkIf cfg.enable {
+              programs.git = {
+                enable = true;
+                userName = cfg.userName;
+                userEmail = cfg.userEmail;
+                ignores = [
+                  ".DS_Store"
+                  "*~"
+                  "\\#*\\#"
+                  ".\\#*"
+                ];
+                extraConfig = {
+                  core.askPass = "";
+                  init.defaultBranch = "main";
+                  submodule.recurse = true;
+                  pull.rebase = false;
+                };
+              };
+            };
+          };
+
       };
     };
 }
