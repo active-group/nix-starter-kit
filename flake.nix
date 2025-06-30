adf7806763fff4712dcc8aff75714f0eb1ad1406
Mike Sperber <sperber@deinprogramm.de>
2025-06-30 14:51:47 +0200

    Handle system with flake-parts.

--- flake.nix
+++ flake.nix
@@ -2,40 +2,58 @@
   description = "A very basic flake";
 
   inputs = {
+    # FIXME: lock
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mac-app-util.url = "github:hraban/mac-app-util";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, mac-app-util }: {
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      flake-parts,
+      mac-app-util,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { config, system, ... }:
+        {
+          legacyPackages = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+      flake = {
+        lib = {
+          make-home-manager-config =
+            pkgs: home-manager: user-settings:
+            home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              modules = (pkgs.lib.optional pkgs.stdenv.isDarwin mac-app-util.homeManagerModules.default) ++ [
+                (import ./home.nix user-settings)
+              ];
+              extraSpecialArgs = { inherit inputs; };
+            };
 
-    lib.make-home-manager-config = pkgs: home-manager: user-settings:
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules =
-          (pkgs.lib.optional pkgs.stdenv.isDarwin mac-app-util.homeManagerModules.default) ++
-          [(import ./home.nix user-settings)];
-        extraSpecialArgs = {inherit inputs;};
+          make-default-home-manager-config =
+            system: user-settings:
+            self.lib.make-home-manager-config self.legacyPackages.${system} home-manager user-settings;
+
+          # temporary
+          make-emacs = import ./emacs;
+          make-git = import ./git;
+          controlling = import ./controlling;
+        };
       };
-
-    lib.make-default-home-manager-config = system: user-settings:
-      self.lib.make-home-manager-config
-        self.legacyPackages.${system}
-        home-manager
-        user-settings;
-    
-    legacyPackages.aarch64-darwin =
-      import nixpkgs {
-        system = "aarch64-darwin";
-        config.allowUnfree = true;
-      };
-
-    # temporary
-    lib.make-emacs = import ./emacs;
-    lib.make-git = import ./git;
-    lib.controlling = import ./controlling;
-  };
+    };
 }
