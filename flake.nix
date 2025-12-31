13bcb98c4a9c63ca1492e95eaf6fe60e2e9c0589
Mike Sperber <sperber@deinprogramm.de>
2025-12-31 10:57:43 +0100

    NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Modules and utilities for getting started with home-manager at Active Group";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mac-app-util = {
